2ddb46027d61f195a1706196b6273c770c975d72
文顶顶 <704905290@qq.com>
2016-01-07 17:35:18 +0800

    初始化项目，该版本为SDWebImage最新版本。完成主要组建的注释工作。

---
+++ SDWebImage框架源码注解-文顶顶/SDWebImage框架源码注解-文顶顶/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDWebImage框架源码注解-文顶顶
+//
+//  Created by 文顶顶 on 16/1/5.
+//  Copyright © 2016年 文顶顶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SDWebImage框架源码注解-文顶顶/SDWebImage框架源码注解-文顶顶Tests/SDWebImage__________Tests.m
@@ -0,0 +1,39 @@
+//
+//  SDWebImage__________Tests.m
+//  SDWebImage框架源码注解-文顶顶Tests
+//
+//  Created by 文顶顶 on 16/1/5.
+//  Copyright © 2016年 文顶顶. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SDWebImage__________Tests : XCTestCase
+
+@end
+
+@implementation SDWebImage__________Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
