fa73e33ab118722f9dccc774035780fa81c19b10
文顶顶 <1915158156@qq.com>
2016-01-09 15:44:03 +0800

    为NSData+ImageContentType.h和NSData+ImageContentType.m文件添加注解

--- SDWebImage框架源码注解-文顶顶/SDWebImage框架源码注解-文顶顶/SDWebImage/NSData+ImageContentType.h
+++ SDWebImage框架源码注解-文顶顶/SDWebImage框架源码注解-文顶顶/SDWebImage/NSData+ImageContentType.h
@@ -13,6 +13,11 @@
  *  @param data the input data
  *
  *  @return the content type as string (i.e. image/jpeg, image/gif)
+ *
+ * 根据图像的二进制数据判断图片的类型
+ *
+ * @param  data 传入的二进制数据
+ * @return 图片类型的字符串（如：image/jpeg,image/gif）
  */
 + (NSString *)sd_contentTypeForImageData:(NSData *)data;
 
@@ -20,7 +25,7 @@
 
 
 @interface NSData (ImageContentTypeDeprecated)
-
+//过期的方法
 + (NSString *)contentTypeForImageData:(NSData *)data __deprecated_msg("Use `sd_contentTypeForImageData:`");
 
 @end
