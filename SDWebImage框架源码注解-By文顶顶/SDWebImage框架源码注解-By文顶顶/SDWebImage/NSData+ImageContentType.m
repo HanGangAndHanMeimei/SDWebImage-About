73d7823025af4ba8cd12e626eac5cebe3118a6fa
wendingding <1915158156@qq.com>
2016-06-13 19:44:27 +0800

    完成判断图片类型文件的注解

--- SDWebImage框架源码注解-By文顶顶/SDWebImage框架源码注解-By文顶顶/SDWebImage/NSData+ImageContentType.m
+++ SDWebImage框架源码注解-By文顶顶/SDWebImage框架源码注解-By文顶顶/SDWebImage/NSData+ImageContentType.m
@@ -10,7 +10,9 @@
 
 + (NSString *)sd_contentTypeForImageData:(NSData *)data {
     uint8_t c;
+    //获得传入的图片二进制数据的第一个字节
     [data getBytes:&c length:1];
+    //在判断图片类型的时候，只匹配第一个字节
     switch (c) {
         case 0xFF:
             return @"image/jpeg";
@@ -22,16 +24,20 @@
         case 0x4D:
             return @"image/tiff";
         case 0x52:
+            //WEBP :是一种同时提供了有损压缩与无损压缩的图片文件格式
             // R as RIFF for WEBP
             if ([data length] < 12) {
                 return nil;
             }
 
+            //获取前12个字节
             NSString *testString = [[NSString alloc] initWithData:[data subdataWithRange:NSMakeRange(0, 12)] encoding:NSASCIIStringEncoding];
+            //如果以『RIFF』开头，且以『WEBP』结束，那么就认为该图片是Webp类型的
             if ([testString hasPrefix:@"RIFF"] && [testString hasSuffix:@"WEBP"]) {
                 return @"image/webp";
             }
 
+            //否则返回nil
             return nil;
     }
     return nil;
